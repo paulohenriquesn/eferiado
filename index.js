0283d3e65e45041235d7fe065e00912f0311fa68
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-08 21:32:13 -0400

    refactor: new returned object shape

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const funcs = require('./src/functions')
 
 const isDateHoliday = (date) => {
     const returnedObject = { 
-        eFeriadoNacional: false,
-        eFeriadoRegional: false,
-        feriadoNacional: null,
-        feriadosRegionais: null
+        nacionais: [],
+        regionais: [],
     };
 
     if (date instanceof Date) {
@@ -15,29 +13,29 @@ const isDateHoliday = (date) => {
         [date] = date.match(/(\d\d\/\d\d)+/);
     }
 
-    const foundNationalHoliday = feriados.nacionais.find(x => {
-        if (x.move === undefined) return x.data == date
+    returnedObject.data = date
+
+    const foundNationalHolidays = feriados.nacionais.filter((feriadoNacional) => {
+        if (feriadoNacional.move === undefined) return feriadoNacional.data === date
 
         const dateObject = {
             day: date.substr(0, 2),
             month: date.substr(3, 4)
         }
-        const moveDate = funcs.getMoveDate(x.move)
+        const moveDate = funcs.getMoveDate(feriadoNacional.move)
         return moveDate.day === dateObject.day && moveDate.month === dateObject.month
     });
 
-    const foundRegionalHolidays = feriados.regionais.filter(x => {
-        return x.data == date
+    const foundRegionalHolidays = feriados.regionais.filter(feriadoRegional => {
+        return feriadoRegional.data === date
     });
 
-    if (foundNationalHoliday) {
-        returnedObject.eFeriadoNacional = true;
-        returnedObject.feriadoNacional = foundNationalHoliday.nome;
+    if (foundNationalHolidays.length) {
+        returnedObject.nacionais = foundNationalHolidays.map(({ nome }) => ({ nome }));
     }
 
     if (foundRegionalHolidays.length) {
-        returnedObject.eFeriadoRegional = true;
-        returnedObject.feriadosRegionais = foundRegionalHolidays.map(({ nome, estado }) => {
+        returnedObject.regionais = foundRegionalHolidays.map(({ nome, estado }) => {
             return { nome,  estado }
         })
     }
